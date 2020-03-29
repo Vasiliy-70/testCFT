dbef373dd58ce66ffd7809cb0ce5bdeb8b3c0ef0
Василий Боровик <borovik_vasiliy@mail.ru>
2020-03-29 20:45:06 +0700

    Удаление дублирования класса Car

---
+++ WindowsFormsApp1/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string BodyStyle { get; set; }
+        public string Image { get; set; }
+        public int Year { get; set; }
+
+        public Car(int id, string make, string model, int year, string bodyStyle, string image)
+        {
+            Id = id;
+            Make = make;
+            Model = model;
+            Year = year;
+            BodyStyle = bodyStyle;
+            Image = image;
+        }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -6,25 +6,6 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public class Car
-    { 
-        public int Id { get; set; } //Идентификационный номер для БД
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public string BodyStyle { get; set; }
-        public string Image { get; set; }
-        public int Year { get; set; }
-
-        public Car(int id, string make, string model, int year, string bodyStyle, string image)
-        {
-            Id = id;
-            Make = make;
-            Model = model;
-            Year = year;
-            BodyStyle = bodyStyle;
-            Image = image;
-        }
-    }
     static class Program
     {
         /// <summary>
